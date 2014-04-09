c793be1e6ee4e5a5de5295a607ee24c5a57470ec
KarolisNo <kano@kano.lt>
2014-04-09 11:23:22 +0100

    add devise

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @restaurants = Restaurant.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "reviews/new"
 
   resources :restaurants do
     resources :reviews
   end
+
+  root to: "home#index"
+  
 end
