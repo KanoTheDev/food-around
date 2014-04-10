4af74f21537889049ce0727fb84b5348225715ea
KarolisNo <kano@kano.lt>
2014-04-10 20:15:32 +0100

    sample data and depenedent reviews are destroyed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Restaurant.delete_all
+Review.delete_all
+
+kfc = Restaurant.create(name: 'KFC', category: 'Fast food', location: 'Baker street, London')
+mac = Restaurant.create(name: 'McDonalds', category: 'Fast food', location: 'Baker street, London')
+brg = Restaurant.create(name: 'Burger King', category: 'Fast food', location: 'Baker street, London') 
+cv = Restaurant.create(name: 'Chicken Village', category: 'Fast food', location: 'Baker street, London') 
+
+Review.create(comment: 'OK', rating: 4, restaurant: kfc)
+kfc.reviews.create(comment: 'Have it your way', rating: 4) 
+brg.reviews.create(comment: 'Superb!', rating: 5) 
+kfc.reviews.create(comment: 'Very nice', rating: 5) 
+mac.reviews.create(comment: "I'm lovin' it", rating: 5) 
